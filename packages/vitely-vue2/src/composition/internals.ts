c6853e66a303bc5cc2698d702249b791c02e74dc
rhangai <hangai.renan@gmail.com>
2022-08-28 13:50:56 -0300

    chore: Removed types

--- packages/vitely-vue2/src/composition/internals.ts
+++ packages/vitely-vue2/src/composition/internals.ts
@@ -1,7 +1,8 @@
-import { getCurrentInstance, InjectionKey } from 'vue';
+import { getCurrentInstance } from 'vue';
 
 export function useSSRContext() {
 	const vm = getCurrentInstance();
+	// @ts-ignore
 	const context = vm!.proxy.$ssrContext;
 	if (!import.meta.env.SSR || !context) {
 		throw new Error(`Must be called from an ssr`);
